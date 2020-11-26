fa98d546f9ced2552c6df203cb049c6570b202b8
xingweizheng 00591739 <xingweizheng@huawei.com>
2020-11-26 16:19:43 +0800

    imporve daemon push and pull unit test

--- daemon/push_test.go
+++ daemon/push_test.go
@@ -17,13 +17,16 @@ import (
 	"context"
 	"testing"
 
+	"github.com/containers/storage"
 	"github.com/containers/storage/pkg/reexec"
 	"github.com/containers/storage/pkg/stringid"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"gotest.tools/assert"
 
 	constant "isula.org/isula-build"
 	pb "isula.org/isula-build/api/services"
+	"isula.org/isula-build/pkg/logger"
 )
 
 type controlPushServer struct {
@@ -45,13 +48,56 @@ func init() {
 
 func TestPush(t *testing.T) {
 	d := prepare(t)
-	pushID := stringid.GenerateNonCryptoID()[:constant.DefaultIDLen]
-	req := &pb.PushRequest{
-		PushID:    pushID,
-		ImageName: "255.255.255.255/no-repository/no-name",
-	}
 	stream := &controlPushServer{}
-	err := d.Daemon.backend.Push(req, stream)
-	assert.ErrorContains(t, err, "error: locating image")
+
+	testCases := []struct {
+		testName    string
+		pushRequest *pb.PushRequest
+	}{
+		{
+			testName: "localNotExist",
+			pushRequest: &pb.PushRequest{
+				PushID:    stringid.GenerateNonCryptoID()[:constant.DefaultIDLen],
+				ImageName: "255.255.255.255/no-repository/no-name",
+			},
+		},
+		{
+			testName: "manifestNotExist",
+			pushRequest: &pb.PushRequest{
+				PushID:    stringid.GenerateNonCryptoID()[:constant.DefaultIDLen],
+				ImageName: "127.0.0.1/no-repository/no-name:latest",
+			},
+		},
+	}
+
+	options := &storage.ImageOptions{}
+	d.Daemon.localStore.CreateImage(stringid.GenerateRandomID(), []string{"127.0.0.1/no-repository/no-name:latest"}, "", "", options)
+
+	for _, tc := range testCases {
+		err := d.Daemon.backend.Push(tc.pushRequest, stream)
+		if tc.testName == "localNotExist" {
+			assert.ErrorContains(t, err, "error: locating image")
+		}
+		if tc.testName == "manifestNotExist" {
+			assert.ErrorContains(t, err, "file does not exist")
+		}
+	}
+
 	tmpClean(d)
+}
+
+func TestPushHandler(t *testing.T) {
+	stream := &controlPushServer{}
+	cliLogger := logger.NewCliLogger(constant.CliLogBufferLen)
+
+	ctx := context.TODO()
+	eg, _ := errgroup.WithContext(ctx)
+	eg.Go(pushMessageHandler(stream, cliLogger))
+	eg.Go(func() error {
+		cliLogger.Print("Push Response")
+		cliLogger.CloseContent()
+		return nil
+	})
+
+	eg.Wait()
 }
