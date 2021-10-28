2f8f5aa8c8444e9d9c39eba2c060e4e9fa4089bc
DCCooper <1866858@gmail.com>
2021-10-28 15:03:04 +0800

    test: optimize load client options and add unit test
    Signed-off-by: DCCooper <1866858@gmail.com>

--- cmd/cli/load_test.go
+++ cmd/cli/load_test.go
@@ -16,6 +16,7 @@ package main
 import (
 	"context"
 	"io/ioutil"
+	"os"
 	"path/filepath"
 	"testing"
 
@@ -117,6 +118,214 @@ func TestRunLoad(t *testing.T) {
 			assert.Equal(t, err != nil, tc.isErr, "Failed at [%s], err: %v", tc.name, err)
 			if err != nil {
 				assert.ErrorContains(t, err, tc.errString)
+			}
+		})
+	}
+}
+
+func TestResolveLoadPath(t *testing.T) {
+	dir := fs.NewDir(t, t.Name())
+	fileWithContent := fs.NewFile(t, filepath.Join(t.Name(), "test.tar"))
+	ioutil.WriteFile(fileWithContent.Path(), []byte("This is test file"), constant.DefaultRootFileMode)
+	emptyFile := fs.NewFile(t, filepath.Join(t.Name(), "empty.tar"))
+
+	defer dir.Remove()
+	defer fileWithContent.Remove()
+	defer emptyFile.Remove()
+
+	type args struct {
+		path string
+		pwd  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "TC-normal load path",
+			args: args{
+				path: fileWithContent.Path(),
+				pwd:  dir.Path(),
+			},
+			want: fileWithContent.Path(),
+		},
+		{
+			name: "TC-empty load path",
+			args: args{
+				pwd: dir.Path(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-empty load file",
+			args: args{
+				path: emptyFile.Path(),
+				pwd:  dir.Path(),
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := resolveLoadPath(tt.args.path, tt.args.pwd)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("resolveLoadPath() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("resolveLoadPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCheckLoadOpts(t *testing.T) {
+	root := fs.NewDir(t, t.Name())
+	defer root.Remove()
+	emptyFile, err := os.Create(filepath.Join(root.Path(), "empty.tar"))
+	assert.NilError(t, err)
+	fileWithContent, err := os.Create(filepath.Join(root.Path(), "test.tar"))
+	assert.NilError(t, err)
+	ioutil.WriteFile(fileWithContent.Name(), []byte("This is test file"), constant.DefaultRootFileMode)
+	baseFile, err := os.Create(filepath.Join(root.Path(), "base.tar"))
+	assert.NilError(t, err)
+	ioutil.WriteFile(baseFile.Name(), []byte("This is base file"), constant.DefaultRootFileMode)
+	libFile, err := os.Create(filepath.Join(root.Path(), "lib.tar"))
+	ioutil.WriteFile(libFile.Name(), []byte("This is lib file"), constant.DefaultRootFileMode)
+
+	type fields struct {
+		path   string
+		loadID string
+		sep    separatorLoadOption
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		wantErr bool
+	}{
+		{
+			name: "TC-normal load options",
+			fields: fields{
+				path: fileWithContent.Name(),
+			},
+		},
+		{
+			name:    "TC-empty load path",
+			wantErr: true,
+		},
+		{
+			name: "TC-empty load file",
+			fields: fields{
+				path: emptyFile.Name(),
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					app:  "app:latest",
+					base: baseFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+		},
+		{
+			name: "TC-separated load with empty app name",
+			fields: fields{
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					base: baseFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with empty dir",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					base: baseFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with invalid app name",
+			fields: fields{
+				path: "invalid:app:name",
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					base: baseFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with empty base tarball",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					base: emptyFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with empty lib tarball",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					base: baseFile.Name(),
+					lib:  emptyFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with same base and lib tarball",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					dir:  root.Path(),
+					base: fileWithContent.Name(),
+					lib:  fileWithContent.Name(),
+				},
+			},
+			wantErr: true,
+		},
+		{
+			name: "TC-separated load with dir not exist",
+			fields: fields{
+				path: "app:latest",
+				sep: separatorLoadOption{
+					dir:  "path not exist",
+					base: baseFile.Name(),
+					lib:  libFile.Name(),
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			opt := &loadOptions{
+				path:   tt.fields.path,
+				loadID: tt.fields.loadID,
+				sep:    tt.fields.sep,
+			}
+			if err := opt.checkLoadOpts(); (err != nil) != tt.wantErr {
+				t.Errorf("loadOptions.checkLoadOpts() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
 	}
