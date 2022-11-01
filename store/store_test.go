0c1f841e13e4cb70ff07e18ddfb53e8927edbe95
daisicheng <daisicheng@huawei.com>
2022-11-01 17:11:19 +0800

    improve some error coverage of unit tests in import_test and store_test

--- store/store_test.go
+++ store/store_test.go
@@ -67,12 +67,39 @@ func TestCleanContainers(t *testing.T) {
 
 	s, err := GetStore()
 	assert.NilError(t, err)
-	s.CreateContainer("", []string{""}, "", "", "", nil)
+	s.CreateContainer("", []string{""}, "", "TC1", "", nil)
 	s.CleanContainers()
 	containers, _ := s.Containers()
 	if len(containers) > 0 {
 		t.Errorf("Failed to clean containers")
 	}
+	s.CreateContainer("", []string{""}, "", "TC2", "", nil)
+	err = os.RemoveAll(filepath.Join(storeOpts.DataRoot, "overlay-layers"))
+	assert.NilError(t, err)
+	s.CleanContainers()
+
+	defer func() {
+		unix.Unmount(filepath.Join(storeOpts.DataRoot, "overlay"), 0)
+		unix.Unmount(filepath.Join(storeOpts.RunRoot, "overlay"), 0)
+		os.RemoveAll(dataDir)
+		os.RemoveAll(runDir)
+	}()
+}
+
+func TestCleanContainer(t *testing.T) {
+	dataDir := "/tmp/lib"
+	runDir := "/tmp/run"
+	storeOpts.DataRoot = filepath.Join(dataDir, "containers/storage")
+	storeOpts.RunRoot = filepath.Join(runDir, "containers/storage")
+
+	s, err := GetStore()
+	assert.NilError(t, err)
+	s.CreateContainer("", []string{""}, "", "TC1", "", nil)
+	err = s.CleanContainer("TC1")
+	assert.NilError(t, err)
+	err = s.CleanContainer("TC2")
+	assert.ErrorContains(t, err, "not a container")
+
 	defer func() {
 		unix.Unmount(filepath.Join(storeOpts.DataRoot, "overlay"), 0)
 		unix.Unmount(filepath.Join(storeOpts.RunRoot, "overlay"), 0)
